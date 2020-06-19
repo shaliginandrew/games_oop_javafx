f9e03ec51eeb9b37b29e39ff40eb32d361151299
shaliginandrew <shaligin.andrew@gmail.com>
2020-06-19 18:08:15 +0300

    Крестики-нолики на JavaFX [#242450]

--- tictactoe/src/test/java/job4j/tictactoe/Logic3TTest.java
+++ tictactoe/src/test/java/job4j/tictactoe/Logic3TTest.java
@@ -67,11 +67,11 @@ public class Logic3TTest {
     }
 
     @Test
-    public void whenHasOWinner() { //  не работает
+    public void whenHasOWinner() {
         Figure3T[][] table = {
-                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
-                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(true, false), new Figure3T(), new Figure3T(true, false)},
+                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
+                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
+                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
         };
         Logic3T login = new Logic3T(table);
         assertThat(login.isWinnerO(), is(true));
